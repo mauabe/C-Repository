8738aa73d8dc90a4e70fad36e2e4be61299f27b2
Mauricio Feldman-Abe <6901736+mauabe@users.noreply.github.com>
2018-07-18 16:54:41 -0700

    Add files via upload

--- Proj5 Craps/Feldman-AbeMCS50-Proj5-CRAPS.c
+++ Proj5 Craps/Feldman-AbeMCS50-Proj5-CRAPS.c
@@ -9,54 +9,63 @@
 
 //Include statements
 #include <stdio.h>
-//#include "stdafx.h"  // Don't have this file, using stdio.h instead.
+// #include "stdafx.h"  // Don't have this file, using stdio.h instead.
 #include <time.h>   // TO use time as seed for random numbers
 #include <stdlib.h>  // To srand (random seader) function
-
-//Constants// Exercise: String and Struct
-
-#include "stdafx.h"				// For IO
-#include <string.h>				// For string functions
-#include <stlib.h>
 
 //Constants
 #define TOP 1
 #define BOTTOM 2
-#define LOWER_LIMIT 100
-#define UPPER_LIMIT 200
-#define ARRAY_MAX_SIZE 20
+#define LOWER_LIMIT 1
+#define UPPER_LIMIT 6
+#define ARRAY_MAX_SIZE 2
 
 // Function prototypes (No need to name paramenters in function prototypes)
-void PrintStudentInfo (int topOrBottom);
+void PrintStudentInfo (int );
+
+int GetRecords ([], char *[], int);
+
+
 void CreateRandomNumbers (int [], int, int, int);
-void PrintRandomNumbers (int [], int);
+//void PrintRandomNumbers (int [], int);
+void GenerateDices (int , int , int );
+
 
 //No Global variables
 
 
 
-//void PrintStudentInfo(int);
-int GetRecords ([], char *[], int);
-PrintRecords(StudentRecType studentRecords[], int numOfRecords)
 
 
 // ========================================
 int main(void)
 {
-    int sampleData [ARRAY_MAX_SIZE];
-    int sampleSize = 10;               // This number must be same or less than ARRAY_MAX_SIZE
-    srand(time(NULL));                    // This function must be used just once for entire program
+    int dice1;
+    int dice2;
+    int sum;
+    
+    
+    //int sampleData [ARRAY_MAX_SIZE];
+    int sampleSize = 2;               // This number must be same or less than ARRAY_MAX_SIZE
+    srand(time(NULL));                 // This function must be used just once for entire program
     
     // Call functions: must provide real variables/constant names
     PrintStudentInfo (TOP);
     CreateRandomNumbers (sampleData, sampleSize, LOWER_LIMIT, UPPER_LIMIT);
-    PrintRandomNumbers (sampleData, sampleSize);
+//    PrintRandomNumbers (sampleData, sampleSize);
+    
+    GenerateDices (int dice1, int dice2, int sum)
+
+    
+    
+    
+    
+    
     PrintStudentInfo (BOTTOM);
     
     return 0;
 }
 //=============================
-
 void PrintStudentInfo (int topOrBottom)
 {
     if (topOrBottom == TOP)
@@ -64,12 +73,87 @@ void PrintStudentInfo (int topOrBottom)
     else if(topOrBottom == BOTTOM)
         printf("Mauricio Feldman-Abe - End of Project 5 \n\n");
 }
+// ========================
+void GenerateDices (int dice1, int dice2, int sum)
+{
+    dice1 = RandNum(1,6);
+    dice2 = RandNum(1,6);
+    sum =  dice1 + dice2;
+    printf ("%d  %d  %d", dice1, dice2, sum )
+}
+
+
+// ============================
+void
+    
+int firstHand []
+
+    
+    
+    int crap = (2 || 3 || 12);
+    int win = (7 || 11);
+    
+
+    do
+    {
+    switch(sum)
+    {
+        case 2:
+            printf("Your dices rolled: %d and %d = %d", dice1, dice2, sum);
+            printf("Sorry, you lost!");
+            break
+        case 3:
+            printf("Your dices rolled: %d and %d = %d", dice1, dice2, sum);
+            printf("Sorry, you lost!");
+            break;
+        case 12:
+            printf("Your dices rolled: %d and %d = %d", dice1, dice2, sum);
+            printf("Sorry, you lost!");
+            break;
+            
+        default 7:
+            printf("Your dices rolled: %d and %d = %d", dice1, dice2, sum);
+            printf("Congrats! You won!");
+            break;
+
+        default 11:
+            printf("Your dices rolled: %d and %d = %d", dice1, dice2, sum);
+            printf("Congrats! You won!");
+            break;
+
+            
+            
+        default 4:
+            printf("Your dices rolled: %d and %d = %d", dice1, dice2, sum);
+            printf("Point is now %d!", sum);
+        default 5:
+            printf("Your dices rolled: %d and %d = %d", dice1, dice2, sum);
+            printf("Point is now %d!", sum);
+        default 6:
+            printf("Your dices rolled: %d and %d = %d", dice1, dice2, sum);
+            printf("Point is now %d!", sum);
+        default 8:
+            printf("Your dices rolled: %d and %d = %d", dice1, dice2, sum);
+            printf("Point is now %d!", sum);
+        default 9:
+            printf("Your dices rolled: %d and %d = %d", dice1, dice2, sum);
+            printf("Point is now %d!", sum);
+        default 10:
+            printf("Your dices rolled: %d and %d = %d", dice1, dice2, sum);
+            printf("Point is now %d!", sum);
+    }
+    }
+    while
+        
+            
+}
+
 
 // ===========
 void CreateRandomNumbers (int sampleData[], int sampleSize, int LL, int UL)
 {
     int i;
-    for (i = 0; i < sampleSize; i++)
+    for (i = 0; i < 2; i++)
         sampleData[i] = (rand() % (UL - LL + 1)) + LL;
 }
 // ==========
